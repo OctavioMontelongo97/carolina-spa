1f581e1694bcac955f333067654c88c25d203a01
Octavio Montelongo <ogmontelongo97@gmail.com>
2021-07-12 21:21:14 -0500

    Finalización proyecto

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// Add Your Scripts here
+//validación de campos en formulario
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Seleccionar el formulario para validar
+      var forms = document.getElementsByClassName("needs-validation");
+      // Validar cada campo y prevenir que se envie
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
